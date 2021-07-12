289b2d6f1af44cbaf8605490557b40a8ba254885
Jack Fahey-Gilmour <33475246+Jack-AFL@users.noreply.github.com>
2021-07-12 14:14:10 +0800

    Create powershell execution.ps1
    For executing docker images

---
+++ powershell execution.ps1
@@ -0,0 +1,13 @@
+﻿cd "C:\Users\jfahey-gilmour\OneDrive - WESTERN AUSTRALIAN INSTITUTE OF SPORT INCORPORATED\Documents\GitHub\docker-test"
+
+docker images
+docker containers
+
+# Build image
+docker build -t waisdatascience/docker-test .
+
+# Run container from image
+
+docker run -e TZ=Australia/Perth waisdatascience/docker-test
+
+docker run -v ~/Data:/Data waisdatascience/docker-test
