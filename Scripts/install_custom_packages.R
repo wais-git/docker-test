ad368d4a57cc1b2cbe37e8f41c5db3eb5d13dc79
Jack Fahey-Gilmour <33475246+Jack-AFL@users.noreply.github.com>
2021-07-22 15:20:29 +0800

    Update install_custom_packages.R

--- Scripts/install_custom_packages.R
+++ Scripts/install_custom_packages.R
@@ -19,4 +19,4 @@ if (length(new_packages) & !identical(new_packages, character(0))) {
 install.packages('neon/neon_0.1.20.tar.gz', repos = NULL)
 
 # WAISR ========================================================================
-remotes::install_github('wais-git/WAISR', auth_token = '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')
+remotes::install_github('wais-git/WAISR', auth_token = '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')
